72c672051398475ba1ab8f077f9c01ac755e48a3
Lokesh Chigulla <124062799+Lokesh-Chigulla@users.noreply.github.com>
2023-03-15 12:32:38 +0530

    Delete student_form directory

--- student_form/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, login_view, signup,main
-
-urlpatterns = [
-    path('', index, name='home'),
-    path('login/',login_view, name='login'),
-    path('signup/', signup, name='signup' ),
-    path('main/', main, name='main')
-]
